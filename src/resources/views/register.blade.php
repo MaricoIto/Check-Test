a8c2d3a855cb86d3f875a83ebfbdb09060c78d2d
Maricos <itomarico15@gmail.com>
2024-08-21 22:53:47 +0900

    ヘッダー部分更新

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/resister.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/register.css') }}" />
 @endsection
 @section('auth_button')
 <a href="{{ route('login') }}" class="btn btn-primary">login</a>
