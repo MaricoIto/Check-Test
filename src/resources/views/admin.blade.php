05ce2fcd26196a790bcf80b8e5fd3f5fad199200
Maricos <itomarico15@gmail.com>
2024-08-23 22:20:59 +0900

    管理画面更新

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -24,39 +24,38 @@
                     <select class="admin__search-form--item-select">
                         <option value="">お問い合わせの種類</option>
                     </select>
-                    <select class="admin__search-form--item-select">
-                        <input type="date">/月/日</input>
-                    </select>
-                    <button type="submit" class="btn-submit">検索</button>
-                    <button type="submit" class="btn-reset">リセット</button>
+                    <input type="date" class="admin__search-form--item-date"></input>
+                    <button type="submit" class="admin__search-form--item-btn--submit">検索</button>
+                    <button type="submit" class="admin__search-form--item-btn--reset">リセット</button>
                 </div>
             </form>
 
             <!-- 出力ボタンとページネーション -->
             <div class="admin__menu">
-                <div class="admin__menu--btn-export">
-                    <button type="submit" class="btn-export">エクスポート</button>
-                    <p>ページネーションの表示</p>
+                <button type="submit" class="admin__btn-export">エクスポート</button>
+                <p>ページネーションの表示</p>
+            </div>
 
-                    <div class="admin__table">
-                        <table>
-                            <tr>
-                                <th>お名前</th>
-                                <th>性別</th>
-                                <th>メールアドレス</th>
-                                <th>お問い合わせの種類</th>
-                                <th></th>
-                            </tr>
-                            <tr>
-                                <td>山田　太郎</td>
-                                <td>男性</td>
-                                <td>test@example.com</td>
-                                <td>商品の交換について</td>
-                                <td><button class="admin__table-btn">詳細</button></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
+            <!-- テーブル -->
+            <div class="admin__table">
+                <table>
+                    <tr>
+                        <th>お名前</th>
+                        <th>性別</th>
+                        <th>メールアドレス</th>
+                        <th>お問い合わせの種類</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>山田　太郎</td>
+                        <td>男性</td>
+                        <td>test@example.com</td>
+                        <td>商品の交換について</td>
+                        <td><button class="admin__table-btn">詳細</button></td>
+                    </tr>
+                </table>
             </div>
         </div>
-        @endsection
+
+    </div>
+    @endsection
