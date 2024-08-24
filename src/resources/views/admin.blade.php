7b172237ad747065b470c662424a79581731f4cb
Maricos <itomarico15@gmail.com>
2024-08-24 12:10:30 +0900

    管理画面更新

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -8,54 +8,49 @@
 
 @section('content')
 <div class="admin">
-    <div class="admin__inner">
 
-        <!-- タイトル -->
-        <p class="admin__title">Admin</p>
+    <!-- タイトル -->
+    <p class="admin__title">Admin</p>
 
-        <!-- 検索フォーム -->
-        <div class="admin__content">
-            <form class="admin__search-form">
-                <div class="admin__search-form--item">
-                    <input type="text" class="admin__search-form-input" placeholder="名前やメールアドレスを入力してください">
-                    <select class="admin__search-form--item-select">
-                        <option value="">性別</option>
-                    </select>
-                    <select class="admin__search-form--item-select">
-                        <option value="">お問い合わせの種類</option>
-                    </select>
-                    <input type="date" class="admin__search-form--item-date"></input>
-                    <button type="submit" class="admin__search-form--item-btn--submit">検索</button>
-                    <button type="submit" class="admin__search-form--item-btn--reset">リセット</button>
-                </div>
-            </form>
+    <!-- 検索フォーム -->
+    <form class="admin__search-form">
+            <input type="text" class="admin__search-form--input" placeholder="名前やメールアドレスを入力してください">
+            <select class="admin__search-form--select">
+                <option value="">性別</option>
+                <option value="男性">男性</option>
+                <option value="女性">女性</option>
+                <option value="その他">その他</option>
+            </select>
+            <select class="admin__search-form--select">
+                <option value="">お問い合わせの種類</option>
+                <option value="商品の交換について">商品の交換について</option>
+            </select>
+            <input type="date" class="admin__search-form--date"></input>
+            <button type="submit" class="admin__search-form--btn-submit">検索</button>
+            <button type="submit" class="admin__search-form--btn-reset">リセット</button>
+    </form>
 
-            <!-- 出力ボタンとページネーション -->
-            <div class="admin__menu">
-                <button type="submit" class="admin__btn-export">エクスポート</button>
-                <p>ページネーションの表示</p>
-            </div>
-
-            <!-- テーブル -->
-            <div class="admin__table">
-                <table>
-                    <tr>
-                        <th>お名前</th>
-                        <th>性別</th>
-                        <th>メールアドレス</th>
-                        <th>お問い合わせの種類</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <td>山田　太郎</td>
-                        <td>男性</td>
-                        <td>test@example.com</td>
-                        <td>商品の交換について</td>
-                        <td><button class="admin__table-btn">詳細</button></td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-
+    <!-- 出力ボタンとページネーション -->
+    <div class="admin__menu">
+        <button type="submit" class="admin__btn-export">エクスポート</button>
+        <p>ページネーションの表示</p>
     </div>
+
+    <!-- テーブル -->
+    <table class="admin__table">
+        <tr>
+            <th>お名前</th>
+            <th>性別</th>
+            <th>メールアドレス</th>
+            <th>お問い合わせの種類</th>
+            <th></th>
+        </tr>
+        <tr>
+            <td>山田　太郎</td>
+            <td>男性</td>
+            <td>test@example.com</td>
+            <td>商品の交換について</td>
+            <td><button class="admin__table--btn">詳細</button></td>
+        </tr>
+    </table>
     @endsection
